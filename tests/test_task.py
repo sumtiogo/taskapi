bc46e6b220af9faa8bc9d70c877128e3088a9f66
Ayson Chang <ayson.chang@evolutivelabs.com>
2022-09-11 14:00:44 +0800

    add test for get tasks given data

--- tests/test_task.py
+++ tests/test_task.py
@@ -1,7 +1,29 @@
+from flask import Flask
 from flask.testing import FlaskClient
+
+from taskapi.db import get_db
 
 
 def test_get(client: FlaskClient):
+    # act
     response = client.get('/tasks')
+    # assertion
     assert response.status_code == 200
     assert response.json == {'result': []}
+
+
+def test_get_given_data(app: Flask, client: FlaskClient):
+    # arrange
+    with app.app_context():
+        get_db().executescript('''
+        INSERT INTO task (status)
+        VALUES (true), (false)
+        ''')
+    # act
+    response = client.get('/tasks')
+    # assertion
+    assert response.status_code == 200
+    assert response.json == {'result': [
+        {'id': 1, 'status': True},
+        {'id': 2, 'status': False},
+    ]}
